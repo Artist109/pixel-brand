a4507b2bdcf5a7c5ce059657a0fc136bbab591d7
Sergey Fadeev <artistsilen@gmail.com>
2022-12-12 02:33:10 +0300

    added checking on existing users

--- src/App/components/itemsList.jsx
+++ src/App/components/itemsList.jsx
@@ -1,48 +1,53 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import api, { fetchItems } from "../api/items";
+import { fetchItems } from "../api/items";
 
 const ItemsList = () => {
   const [items, setItems] = useState(fetchItems());
-  console.log();
-  return (
-    <>
-      <div className="m-4">
-        <div className="card-group row row-cols-4 g-0">
-          {items.map((item) => {
-            return (
-              <div class="col">
-                <div className="card h-100">
-                  <img
-                    src={item.image}
-                    className="card-img-top"
-                    alt={item.title}
-                    style={{ maxWidth: "200" + "px", maxHeight: "200px" }}
-                  ></img>
-                  <div className="card-body">
-                    <h5 className="card-title">{item.title}</h5>
-                    <p
-                      className="card-text"
-                      style={{ textDecoration: "line-through" }}
-                    >
-                      {item.price}
-                    </p>
-                    <p className="card-text">{item["new-price"]}</p>
-                    <p className="card-text">
-                      <small className="text-muted">Курьером 28 декабря</small>
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      В корзину
-                    </a>
+  if (items) {
+    return (
+      <>
+        <div className="m-4">
+          <div className="card-group row row-cols-4 g-0">
+            {items.map((item) => {
+              return (
+                <div class="col">
+                  <div className="card h-100">
+                    <img
+                      src={item.image}
+                      className="card-img-top"
+                      alt={item.title}
+                      style={{ maxWidth: "200" + "px", maxHeight: "200px" }}
+                    ></img>
+                    <div className="card-body">
+                      <h5 className="card-title">{item.title}</h5>
+                      <p
+                        className="card-text"
+                        style={{ textDecoration: "line-through" }}
+                      >
+                        {item.price}
+                      </p>
+                      <p className="card-text">{item["new-price"]}</p>
+                      <p className="card-text">
+                        <small className="text-muted">
+                          Курьером 28 декабря
+                        </small>
+                      </p>
+                      <a href="#" class="btn btn-primary">
+                        В корзину
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return <h3>...Загрузка</h3>;
+  }
 };
 
 export default ItemsList;
